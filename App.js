a2ac92001efdcacc51d0335eba40a1b9a27976d0
Satyam Govind Rao <satyamgrao007@gmail.com>
2024-05-12 13:45:54 +0530

    updated the files

--- App.js
+++
@@ -1,8 +0,0 @@
-const parent = React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"Child"},
-React.createElement("h1",{id:"heading"},"i am the H1 tag")));
-
-
-
-const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
